69dc01842026a9e6d8ac66c3499e4325a4af2cc7
Shad Ullah Khan <shaduk@gmail.com>
2016-01-03 13:30:35 +0000

    added code for missing letter day 4

---
+++ missing-letter.js
@@ -0,0 +1,27 @@
+/* Find the missing letter in the passed letter range and return it.
+
+If all letters are present in the range, return undefined.
+*/
+
+function fearNotLetter(str) {
+  var first = str[0].charCodeAt(0);
+  var ans;
+  for(var i = 0; i < str.length; i++)
+    {
+       if(String.fromCharCode(first) === str[i])
+         {
+            first = first + 1;
+         }
+       else
+         {
+            ans = String.fromCharCode(first);
+         }
+       
+  
+          
+        
+    }
+  return ans;
+}
+
+fearNotLetter("abce");
